394fb9403d292d979b767123dfc902a1f66fee47
Marixa82 <121823496+Marixa82@users.noreply.github.com>
2023-12-30 20:47:47 +0200

    upd4

--- src/4.ts
+++ src/4.ts
@@ -1,3 +1,52 @@
+class Key {
+    private signature: number = Math.random();
+
+    getSignature(): number {
+        return this.signature;
+    }
+}
+
+class Person {
+    private key: Key;
+
+    constructor(initialKey: Key) {
+        this.key = initialKey;
+    }
+
+    getKey(): Key {
+        return this.key;
+    }
+}
+
+abstract class House {
+    door: boolean = false;
+    key: Key;
+    tenants: Person[] = [];
+
+    comeIn(tenant: Person) {
+        if (this.door === true) {
+            this.tenants.push(tenant);
+           } 
+    }
+
+    abstract openDoor(key: Key): void;
+}
+
+class MyHouse extends House {
+    constructor(key: Key) {
+        super();
+        this.key = key;
+    }
+
+    openDoor(enteredKey: Key): void {
+        if (enteredKey.getSignature() === this.key.getSignature()) {
+            this.door = true;
+           
+        } 
+    }
+}
+
+
 const key = new Key();
 
 const house = new MyHouse(key);
